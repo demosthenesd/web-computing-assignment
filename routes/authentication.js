02ab6e42c1a1562088155f4bb5f9c588086bd175
demosthenesd <demosthenesd@icloud.com>
2022-05-27 16:15:28 +1000

    authentication changed to router.post

--- routes/authentication.js
+++ routes/authentication.js
@@ -2,15 +2,12 @@ var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
-});
 
-router.get("/register", function (req, res, next) {
+router.post("/register", function (req, res, next) {
   res.send("DIS REGISTER AUTHENTICATION");
 });
 
-router.get("/login", function (req, res, next) {
+router.post("/login", function (req, res, next) {
   res.send("DIS LOGIN AUTHENTICATION ");
 });
 
