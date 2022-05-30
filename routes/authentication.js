1dffc07de96e46101adf7d4080c8c18145dba3c1
JRD <demosthenesd@icloud.com>
2022-05-30 17:25:04 +1000

    fixed returns on register

--- routes/authentication.js
+++ routes/authentication.js
@@ -22,23 +22,24 @@ router.post("/register", function (req, res, next) {
     .select("*")
     .where("email", "=", email);
 
-  queryUsers
-    .then((users) => {
-      if (users.length > 0) {
-        res.status(409).json({
-          error: true,
-          message: "User already exists",
-        });
-        return;
-      }
+  queryUsers.then((users) => {
+    if (users.length > 0) {
+      res.status(409).json({
+        error: true,
+        message: "User already exists",
+      });
+      return;
+    }
 
-      const saltRounds = 10;
-      const hash = bcrypt.hashSync(password, saltRounds);
-      return req.db.from("users").insert({ email, hash });
-    })
-    .then(() => {
-      res.status(201).json({ message: "User created" });
-    });
+    const saltRounds = 10;
+    const hash = bcrypt.hashSync(password, saltRounds);
+    req.db
+      .from("users")
+      .insert({ email, hash })
+      .then(() => {
+        res.status(201).json({ message: "User created" });
+      });
+  });
 });
 
 router.post("/login", function (req, res, next) {
