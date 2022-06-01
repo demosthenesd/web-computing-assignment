a0dc69612435aa467c93ad7a1d3923b957d09be5
JRD <demosthenesd@icloud.com>
2022-06-01 14:51:39 +1000

    Ask tute how to check isNAN in volcano. js

--- routes/volcano.js
+++ routes/volcano.js
@@ -7,9 +7,9 @@ var query;
 
 router.get("/volcano/:id", auth, function (req, res, next) {
   if (isNaN(req.params.id)) {
-    res.status(404).json({
+    res.status(400).json({
       error: true,
-      message: `Volcano with ID: ${req.params.id} not found.`,
+      message: "Invalid query parameters. Query parameters are not permitted.",
     });
     return;
   }
@@ -34,25 +34,17 @@ router.get("/volcano/:id", auth, function (req, res, next) {
       .where("id", "=", req.params.id);
   }
 
-  query
-    .then((rows) => {
-      if (rows.length === 0) {
-        res.status(404).json({
-          error: true,
-          message: `Volcano with ID: ${req.params.id} not found.`,
-        });
-        return;
-      }
-
-      res.json(rows[0]);
-    })
-    .catch((err) => {
-      res.status(400).json({
+  query.then((rows) => {
+    if (!rows.length) {
+      res.status(404).json({
         error: true,
-        message:
-          "Invalid query parameters. Query parameters are not permitted.",
+        message: `Volcano with ID: ${req.params.id} not found.`,
       });
-    });
+      return;
+    }
+
+    res.json(rows[0]);
+  });
 });
 
 module.exports = router;
