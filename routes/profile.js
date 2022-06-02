0fe35b6d1264fbdc41b1a8b71a5b287939c2352e
demosthenesd <demosthenesd@icloud.com>
2022-06-02 18:40:57 +1000

    Update profile.js

--- routes/profile.js
+++ routes/profile.js
@@ -37,7 +37,8 @@ router.put("/:email/profile", auth, function (req, res, next) {
   const dob = req.body.dob;
   const address = req.body.address;
 
-  console.log(req.decoded.email);
+  let lettersOnly = /^[A-Za-z]+$/;
+  let emailFormat = /^[^@]+@[^@]+\.[^@]+$/;
 
   if (!firstName || !lastName || !dob || !address) {
     res.status(400).json({
@@ -46,8 +47,18 @@ router.put("/:email/profile", auth, function (req, res, next) {
         "Request body incomplete: firstName, lastName, dob and address are required.",
     });
     return;
+  } else if (
+    !lettersOnly.test(firstName) ||
+    !lettersOnly.test(lastName) ||
+    typeof address !== "string"
+  ) {
+    res.status(400).json({
+      error: true,
+      message:
+        "Request body invalid: firstName, lastName and address must be strings only.",
+    });
+    return;
   }
-
   if (!req.checkAuth) {
     res.status(401).json({
       error: true,
@@ -73,7 +84,11 @@ router.put("/:email/profile", auth, function (req, res, next) {
     )
     .where({ email: req.params.email });
 
-  query.then(() => res.status(200).json({ firstName, lastName, dob, address }));
+  query.then(() =>
+    res
+      .status(200)
+      .json({ email: req.body.email, firstName, lastName, dob, address })
+  );
 });
 
 module.exports = router;
